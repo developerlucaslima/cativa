e1ae09d41f48bb45e0970dff56e137c16643e82e
devlucaslima <developerlucaslima@gmail.com>
2024-06-23 16:55:01 -0300

    ✨ feat: server

---
+++ cativa-server/src/cativa-server.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express'
+import { routes } from './routes'
+import { sqliteConnection } from './database/sqlite'
+import { AppError } from './error/app-error'
+
+sqliteConnection()
+const app = express()
+const PORT = 3000
+app.use(express.json())
+app.use(routes)
+
+app.use((error: Error, _: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
+app.listen(PORT, () =>
+  console.log(`⚒️ O servidor esta rodando na porta ${PORT}`),
+)
