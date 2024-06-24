b8bf78ebde61e455d9906473aa14d2eb8598df56
devlucaslima <developerlucaslima@gmail.com>
2024-06-24 13:36:59 -0300

    ✨ feat: icons

---
+++ cativa-client/src/config/icons.tsx
@@ -0,0 +1,8 @@
+import { MdDeleteForever, MdModeEdit } from 'react-icons/md'
+
+export const editIcon = (
+  <MdModeEdit style={{ color: '#5388ce', fontSize: '1.2rem' }} />
+)
+export const deleteIcon = (
+  <MdDeleteForever style={{ color: '#d85c56', fontSize: '1.25rem' }} />
+)
