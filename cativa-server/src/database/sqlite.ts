46adc624e556d42b57c22f367b04948f3de06c86
devlucaslima <developerlucaslima@gmail.com>
2024-06-24 13:26:23 -0300

    🗃️ chore: create db

--- cativa-server/src/database/sqlite.ts
+++ cativa-server/src/database/sqlite.ts
@@ -1,12 +1,14 @@
-import sqlite3 from 'sqlite3' // version of the driver used
-import sqlite from 'sqlite' // used to establish connection with the db
+import sqlite3 from 'sqlite3'
+import { open, Database } from 'sqlite'
 import path from 'path'
+import { agenciesTable } from './agencies.table'
 
-export async function sqliteConnection() {
-  const database = await sqlite.open({
-    filename: path.resolve(__dirname, '..', 'database.db'),
+export async function sqliteConnection(): Promise<Database> {
+  const database = await open({
+    filename: path.resolve(__dirname, '../database', 'cativa-db.db'),
     driver: sqlite3.Database,
   })
 
+  await agenciesTable(database)
   return database
 }
