25a7acd8f77eef16bf52eb5d7da6c8e9d0f8b9e0
devlucaslima <developerlucaslima@gmail.com>
2024-06-24 13:32:13 -0300

    ✨ feat: agencies routes new route getAll

--- cativa-server/src/routes/agencies-routes.ts
+++ cativa-server/src/routes/agencies-routes.ts
@@ -4,7 +4,8 @@ import { Router } from 'express'
 export const agenciesRoutes = Router()
 const agenciesController = new AgenciesController()
 
-agenciesRoutes.post('/', agenciesController.create)
-agenciesRoutes.get('/:id', agenciesController.getById)
-agenciesRoutes.put('/:id', agenciesController.update)
+agenciesRoutes.post('/add', agenciesController.create)
+agenciesRoutes.get('/', agenciesController.getAll)
+agenciesRoutes.get('/getById/:id', agenciesController.getById)
+agenciesRoutes.put('/update/:id', agenciesController.update)
 agenciesRoutes.delete('/:id', agenciesController.delete)
