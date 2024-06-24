7d62c484cb190b0d556bdc1f03c41f345b9aa707
devlucaslima <developerlucaslima@gmail.com>
2024-06-24 15:23:18 -0300

    ✨ feat: navbar items (+ agência)

--- cativa-client/src/config/navbarItems.ts
+++ cativa-client/src/config/navbarItems.ts
@@ -1,3 +1,3 @@
 export const NavbarItems = () => [
-  { id: 2, text: 'Associados', link: `/associados`, delay: 0.2, target: '' },
+  { id: 2, text: '+ Agência', link: `/add`, delay: 0.2, target: '' },
 ]
